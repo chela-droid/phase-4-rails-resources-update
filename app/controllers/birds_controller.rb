db879ff65030964976b4d102dee5d0dd08cad294
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-03-24 16:46:45 +0300

    not complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,5 @@
 class BirdsController < ApplicationController
-
+  
   # GET /birds
   def index
     birds = Bird.all
@@ -27,5 +27,17 @@ class BirdsController < ApplicationController
   def bird_params
     params.permit(:name, :species)
   end
+
+   # PATCH /birds/:id
+   def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  
 
 end
